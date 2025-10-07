4c8d4bfe2600aa48d37500420d37e661c86ab6d3
turdiyevanvar1403@gmail.com <bekzodhabibullayev68@gmail.com>
2025-10-07 12:59:18 +0500

    Initial commit: Create interactive hello world program

---
+++ gtm.py
@@ -0,0 +1,45 @@
+# first_name = "Anvar"
+# last_name = "Turdiyev"
+# student_id = 20230001
+# gpa = 3.85
+# is_scholarship_eligible = True
+
+# # You can change variable values
+# gpa = 3.92  # Updated GPA
+
+# # Display the information
+# print(f"Student: {first_name} {last_name}")
+# print(f"ID: {student_id}")
+# print(f"GPA: {gpa}")
+
+
+#new script 
+# name="Anvar"
+# age=18
+# print("my name is" + name + "and i am " + str(age) + "years old")
+
+#new script about PRICE
+# 
+# price = 15999.5
+# print(f"Price: {price:.2f} sum")
+
+#SCRIPT 4
+
+# print(f"\n{'='*50}")
+
+#exercises 1
+
+# a=float(input("write a temperature in Celcius: "))
+# fahrenheit= (a * (9/5))+32
+# Kelvin = float(a + 273.15)
+# print(f"{fahrenheit:.2f} fahrenheit")
+# print(f"{Kelvin:.2f} Kelvin")
+
+#exercise 2
+
+r=float(input("Write the radius; "))
+pi=3.14159
+C=2*r*pi
+A=pi*r**2
+print(f"Circumference:{C:.2f} cm")
+print(f"area:{A:.2f} cm**2")
